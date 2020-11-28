6a46030e63143fd7eed8e24ed19a2e9c3e5f9a41
Gento <a551.return@gmail.com>
2020-11-28 23:26:21 +0900

    vue3ではvue-router/nextを使わないとダメみたい

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,53 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+  <the-navigation @set-page="setActivePage"></the-navigation>
+  <main>
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
-    TheHeader,
-    BadgeList,
-    UserInfo,
+    TheNavigation,
   },
   data() {
     return {
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
+      activePage: 'teams-list',
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
+        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+      ],
     };
+  },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
+  },
+  methods: {
+    setActivePage(page) {
+      this.activePage = page;
+    },
   },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 html {
   font-family: sans-serif;
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import BaseBadge from './components/BaseBadge.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 
-const app = createApp(App);
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+        { path: '/users', component: UsersList },
+    ]
+})
 
-app.component('base-badge', BaseBadge);
+const app = createApp(App)
+
+app.use(router);
 
 app.mount('#app');
