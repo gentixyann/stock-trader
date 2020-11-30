14262461866117996eb55ca3635f83d3a0e44f05
Gento <a551.return@gmail.com>
2020-11-30 22:18:51 +0900

    gettersを使用

--- src/main.js
+++ src/main.js
@@ -17,6 +17,21 @@ const store = createStore({
         increase(state, payload) {
             state.counter = state.counter + payload.value;
         }
+    },
+    getters: {
+        finalCounter(state) {
+            return state.counter * 2;
+        },
+        normalizeCounter(state) {
+            const finalCounter = state.counter * 3;
+            if (finalCounter < 0) {
+                return 0;
+            }
+            if (finalCounter > 100) {
+                return 100;
+            }
+            return finalCounter;
+        }
     }
 });
 
