c7aa6f7067552393b629f55aab151b353a09f75c
Gento <a551.return@gmail.com>
2020-12-27 13:29:38 +0900

    ページ遷移の検証完了

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,6 +3,10 @@
         FILTER
     </section>
     <section>
-        LOST OF COACHES
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach</router-link>
+        </div>
+        <ul>LOST OF COACHES</ul>
     </section>
 </template>
